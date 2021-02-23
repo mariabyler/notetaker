0d2962c1850d5946f939b6bf4bd1e09c9ff2a2aa
Maria Byler <mariabyler98@gmail.com>
2021-02-22 21:40:30 -0500

    server.js fix, making public connection

--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -1,11 +1,7 @@
-
 var path = require("path");
 module.exports = function(app) {
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
-  });
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT || 2020;
+var PORT = process.env.PORT || 8080;
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 require('./routes/apiRoutes')(app);
