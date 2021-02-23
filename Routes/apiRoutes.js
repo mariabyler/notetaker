ef3efae170286a41ad04c01a6e79f56543b0e71d
Maria Byler <mariabyler98@gmail.com>
2021-02-22 21:49:51 -0500

    trying to fix apiroutes

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,18 +1,38 @@
-var tableData = require("../data/tableData");
-
+const fs = require("fs");
+var notesData = getNotes();
+function getNotes() {
+  let data = fs.readFileSync('./db/db.json');
+  let notes = JSON.parse(data);
+  for (let i = 0; i < notes.length; i++) {
+      notes[i].id = '' + i;
+  }
+  return notes;
+}
 module.exports = function(app) {
-
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
+ app.get("/api/notes", function(req, res){
+  notesData = getNotes();
+  res.json(notesData);
+ });
+ app.post("/api/notes", function(req, res) {
+  let newId = notesData.length;
+  Object.assign(req.body, {"id": newId.toString()});
+  notesData.push(req.body);
+  fs.writeFile('db/db.json', JSON.stringify(notesData), (error) => {
+    if(error) throw error;
+    console.log("file saved: ", notesData);
   });
-
-  app.post("/api/tables", function(req, res) {
-    tableData.push(req.body);
-    res.json(true);
+  res.json(notesData);
+});
+app.delete("/api/notes/:id", function(req, res){
+  console.log("delete this")
+  const requestID = req.params.id;
+  console.log("request id: ", requestID);
+  notesData = notesData.filter(note => {
+    return note.id !== requestID ? note : null ;
   });
-
-  app.post("/api/clear", function(req, res) {
-    tableData.length = 0;
-    res.json({ ok: true });
+  fs.writeFile('db/db.json', JSON.stringify(notesData), (error) => {
+    if(error) throw error;
+    console.log("file deleted: ", notesData);
   });
+});
 };
